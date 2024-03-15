d457e6d43f4343c5df2c638237efe4ecd16e1760
Shewitgebreyesus <shewitgebreyesus76@gmail.com>
2024-03-15 10:35:53 +0300

    pom folder

---
+++ src/main/java/com/democompany/maven_first/App.java
@@ -0,0 +1,13 @@
+package com.democompany.maven_first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
